7256fe05e2cc46c998fa369a4e1d51505d2b4702
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-11-10 11:19:57 +0100

    Total now works

--- src/components/main/simpleShop/Basket.jsx
+++ src/components/main/simpleShop/Basket.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 
 function Basket(props) {
+
+    function getTotal () {
+        let total = 0
+        props.cart.forEach(item =>{
+            total += item.amount * item.price
+        })
+        return total
+    }
+
   return (
     <section className="Basket ">
       <ul>
         {props.cart.map((item) => (
           <>
             <li>{item.productdisplayname}</li>
-            <li>{"Quantity: "}{item.amount}</li>
+            <li>Quantity: {item.amount}</li>
+            <li>Price: {item.price} DKK</li>
           </>
         ))}
       </ul>
+      <h3>Total: {getTotal()} DKK</h3>
       <button>Buy Now</button>
     </section>
   );
